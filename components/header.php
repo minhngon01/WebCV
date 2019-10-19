79d2c496bc676a31268c65abeab34d9a4b6dd039
Ngôn Đặng <minhngon01@gmail.com>
2019-10-19 17:03:29 +0700

    first commit

---
+++ components/header.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="./images/for-light-bg.svg" alt="">
+        </a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Templates </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Examples</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Guides</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <button class="btn button">
+                            Sign up
+                        </button>
+                    </a>
+                </li>
+            </ul>   
+        </div>
+</nav>
---
+++ components/home_hero.php
@@ -0,0 +1,21 @@
+<div class = "home-hero-container">
+    <div class = "home-hero-main">
+        
+        <div class = "home-hero-maincontent">
+            <h1 class="home-hero-title">Only 2% of resumes make it past the first round. Be in the top 2%</h1>
+            <p class="home-hero-subtitle">Use professional field-tested resume templates that follow
+            the exact ‘resume rules’ employers look for. Easy to use &amp; done within minutes
+            - try now for free!</p>
+            <div class="home-hero-cta">
+                <div class="home-hero-button">
+                    <a class="home-button button" href="/create-resume">Create My Resume</a>
+                </div>
+            </div>
+
+            <a class="home-hero-resume is-image-loaded">
+            </a>
+        </div>
+        
+    </div>
+</div>
+
