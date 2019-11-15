568f9dc6b0da19514400d5af8c05cce65dcf0c8b
LeHuynhKhuongDuy <khuongduy349@gmail.com>
2019-11-15 15:44:09 +0700

    thay đổi index.php cùng với header.php footer.php để dễ code hơn(thêm biến page)

--- components/footer.php
+++ components/footer.php
@@ -1,103 +1,104 @@
-<footer>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 col-md-2">
-                <p>Connect with us
-                        on social media</p>
-                <div class="socical_network">
-                    <div class="social">
-                        <i class="fab fa-facebook-f"></i>
-                    </div>
-                    <div class="social">
-                        <i class="fab fa-instagram"></i>
-                    </div>
-                    <div class="social">
-                        <i class="fab fa-linkedin"></i>
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="col-12 col-md-2">
-                <h5> JOB SEEKERS</h5>
-                <div class="Content">
-                    <div class="item">
-                        <a href="#">
-                                JOB SEEKERS
-                                Build a Resume</a>
-                    </div>
-                    <div class="item">
-                        <a href="">
-                            Resume example</a>
-                    </div>
-                    <div class="item">
-                        <a href="">Job Search</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-2">
-                <h5> CAREER RESOURCES</h5>
-                <div class="Content">
-                    <div class="item">
-                        <a href="">How to Write a Resume</a>
-                    </div>
-                    <div class="item">
-                        <a href="">Is It Time for a Career Change?</a>
-                    </div>
-                    <div class="item">
-                        <a href="">How to Write a Cover Letter That Lands You an Interview</a>
-                    </div>
-                    <div class="item">
-                        <a href=""> Guides</a>
-                    </div>
-                    <div class="item">
-                        <a href="">Blog</a>
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="col-12 col-md-2">
-                <h5>OUR COMPANY</h5>
-                <div class="Content">
-                    <div class="item">
-                        <a href=""> About Us</a>
-                    </div>
-                    <div class="item">
-                        <a href="">Product Updates</a>
-                    </div>
-                    <div class="item">
-                        <a href="">Pricing</a>
-                    </div>
-                    <div class="item">
-                        <a href=""> Media Kit</a>
-                    </div>
-                    <div class="item">
-                        <a href="">Affiliates</a>
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="col-12 col-md-2">
-                <h5>SUPPORT</h5>
-                <div class="Content">
-                    <div class="item">
-                        <a href="">FAQ</a>
-                    </div>
-                    <div class="item">
-                        <a href="">Contact Us</a>
-                    </div>
-                    <div class="item">
-                        <a href="">Terms of Service</a>
-                    </div>
-                    <div class="item">
-                        <a href="">Disclaimer</a>
-                    </div>
-                    <div class="item">
-                        <a href="">Privacy</a>
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
-    </div>
-
-</footer>
+	<footer>
+	    <div class="container-fluid">
+	        <div class="row">
+	            <div class="col-12 col-md-2">
+	                <p>Connect with us
+	                        on social media</p>
+	                <div class="socical_network">
+	                    <div class="social">
+	                        <i class="fab fa-facebook-f"></i>
+	                    </div>
+	                    <div class="social">
+	                        <i class="fab fa-instagram"></i>
+	                    </div>
+	                    <div class="social">
+	                        <i class="fab fa-linkedin"></i>
+	                    </div>
+	                    
+	                </div>
+	            </div>
+	            <div class="col-12 col-md-2">
+	                <h5> JOB SEEKERS</h5>
+	                <div class="Content">
+	                    <div class="item">
+	                        <a href="#">
+	                                JOB SEEKERS
+	                                Build a Resume</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">
+	                            Resume example</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">Job Search</a>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-12 col-md-2">
+	                <h5> CAREER RESOURCES</h5>
+	                <div class="Content">
+	                    <div class="item">
+	                        <a href="">How to Write a Resume</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">Is It Time for a Career Change?</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">How to Write a Cover Letter That Lands You an Interview</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href=""> Guides</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">Blog</a>
+	                    </div>
+	                    
+	                </div>
+	            </div>
+	            <div class="col-12 col-md-2">
+	                <h5>OUR COMPANY</h5>
+	                <div class="Content">
+	                    <div class="item">
+	                        <a href=""> About Us</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">Product Updates</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">Pricing</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href=""> Media Kit</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">Affiliates</a>
+	                    </div>
+	                    
+	                </div>
+	            </div>
+	            <div class="col-12 col-md-2">
+	                <h5>SUPPORT</h5>
+	                <div class="Content">
+	                    <div class="item">
+	                        <a href="">FAQ</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">Contact Us</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">Terms of Service</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">Disclaimer</a>
+	                    </div>
+	                    <div class="item">
+	                        <a href="">Privacy</a>
+	                    </div>
+	                    
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</footer>
+  </body>
+</html>
