b933ac2453035014769ac86ca784f9c37c187b4e
minhngon01 <minhngon01@gmail.com>
2019-11-24 09:33:13 +0700

    linh tinh

--- components/checkdata.php
+++ components/checkdata.php
@@ -1,16 +1,17 @@
 <?php
-    $connect = mysqli_connect("localhost", "root", "root", "assignment");
+    require_once('model.php');
+    $db_handle = new DBController();
     if(isset($_POST["user_name"]))
     {
         $username = $_POST["user_name"];
-        $query = "SELECT * FROM user_info WHERE username = '".$username."'";
-        $result = mysqli_query($connect, $query);
-        $row = mysqli_fetch_array($result);
+        $sql_query = "SELECT * FROM user_info WHERE username = '".$username."'";
+        $result = $db_handle->runQuery($sql_query);
+        $row = $db_handle->numRows($sql_query);
         if($row > 0){
-            echo "<span class='status-not-available'> Username Not Available.</span>";
+            echo "<div class='status-not-available text-danger'> Username Not Available.</div>";
         }
         else {
-            echo "<span class='status-available'> Username Available.</span>";
+            echo "<div class='status-available text-success' > Username Available.</div>";
         }
     }
     exit();
